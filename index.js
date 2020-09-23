a289e7e76ec10d1ebe622094c54a695e5c586680
Thanith <nathan.boutte@viacesi.fr>
2020-09-23 18:02:44 +0200

    adding debounce component

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 module.exports.Selector = require('./lib/Selector.vue').default
+module.exports.Debounce = require('./lib/debounce/Debounce.vue').default
---
+++ lib/debounce/Debounce.vue
@@ -0,0 +1,41 @@
+<script>
+import debounce from './debounce'
+
+const patchValueDebounced = debounce(function (value) {
+  this.$emit('input', value)
+}, 200)
+
+export default {
+  props: {
+    value: {
+      type: [Object, String, Number, Boolean],
+      required: true,
+    },
+  },
+
+  data: () => ({
+    debounced: null,
+  }),
+
+  watch: {
+    debounced: patchValueDebounced,
+  },
+
+  mounted() {
+    this.debounced = this.value
+  },
+
+  methods: {
+    input(val) {
+      this.debounced = val
+    },
+  },
+
+  render() {
+    return this.$scopedSlots.default({
+      debounced: this.debounced,
+      debounce: this.input,
+    })
+  },
+}
+</script>
