174c73e0b1787d68b02d3c959a5b502d0a2456e7
Thanith <nathan.boutte@viacesi.fr>
2020-10-07 10:52:38 +0200

    adding delayed to index

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.Selector = require('./lib/Selector.vue').default
+module.exports.Delayed = require('./lib/Delayed.vue').default
 module.exports.Debounce = require('./lib/debounce/Debounce.vue').default
