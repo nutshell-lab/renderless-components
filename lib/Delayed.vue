597471df18cd3d95ce24fd215878a44f76683f1b
Thanith <nathan.boutte@viacesi.fr>
2020-10-07 10:43:26 +0200

    add delayed comp

---
+++ lib/Delayed.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    duration: {
+      type: Number,
+      default: () => 1000,
+    },
+  },
+  data: () => ({
+    show: false,
+  }),
+  mounted() {
+    setTimeout(() => (this.show = true), this.duration)
+  },
+  render() {
+    return this.$scopedSlots.default({
+      show: this.show,
+    })
+  },
+}
+</script>
